c71f33e7fb305249a9dbfbe135a98be9b52db4e0
algayalm <mudar.algayal@helsinki.fi>
2023-03-19 12:57:28 +0200

    initial commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask import redirect, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+ 
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///algayalm"
+db = SQLAlchemy(app)
+
+@app.route("/")
+def index():
+    result = db.session.execute("SELECT content FROM messages")
+    messages = result.fetchall()
+    print(messages)
+    return render_template("index.html", count=len(messages), messages=messages) 
+
+@app.route("/new")
+def new():
+    return render_template("new.html")
+
+@app.route("/send", methods=["POST"])
+def send():
+    content = request.form["content"]
+    sql = "INSERT INTO messages (content) VALUES (:content)"
+    db.session.execute(sql, {"content":content})
+    db.session.commit()
+    return redirect("/")
